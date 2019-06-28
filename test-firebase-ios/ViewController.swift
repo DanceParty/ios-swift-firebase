f2a069556bdcb4ff2061dd54e327259c0c972ac1
Keevan Dance <jodance@microsoft.com>
2019-06-28 10:42:34 -0700

    Initial Commit

---
+++ test-firebase-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-firebase-ios
+//
+//  Created by Keevan Dance on 6/28/19.
+//  Copyright © 2019 Keevan Dance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
